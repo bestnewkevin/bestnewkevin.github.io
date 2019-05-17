f54bb0597897869fcda415f4b278cfed7c3040c1
Kev Kev <kn1120@nyu.edu>
2019-05-17 13:48:04 -0400

    adding matcher

--- scripts/matching.js
+++ scripts/matching.js
@@ -1,75 +1,99 @@
-/*
+/*https://www.taniarascia.com/how-to-create-a-memory-game-super-mario-with-plain-javascript/*/
 
-https://scotch.io/tutorials/how-to-build-a-memory-matching-game-in-javascript#toc-process-flow
+/* thanks tania* <3*/
 
-<3 also joe versoza my AIT professor
-*/
 
-let card = document.getElementsByClassName("card");
-let cards = [...card];
-// loop to add event listeners to each card
-for (var i = 0; i < cards.length; i++){
-   cards[i].addEventListener("click", displayCard);
-};
-//displayCard is a function we'll talk about this soon
-var openedCards = [];
+const cardsArray = [
+    {
+      name: "strokes",
+      img: "images/matching/isthisit.png",
+    },
+    {
+      name: "iceage",
+      img: "images/matching/beyondless.jpg",
+    },
+    {
+      name: "smtb",
+      img: "images/matching/dogwhistle.jpg",
+    },
 
-var displayCard = function (){
-    this.classList.toggle("open");
-    this.classList.toggle("show");
-    this.classList.toggle("disabled");
- }
+    {
+      name: "anco",
+      img: "images/matching/feels.jpg",
+    },
+    {
+      name: "melody",
+      img: "images/matching/melody.jpg",
+    }, 
+    {
+      name: "steve",
+      img: "images/matching/apolloxxi.jpg",
+    },
+    
 
- function cardOpen() {
-    openedCards.push(this);
-    var len = openedCards.length;
-    if(len === 2){
+  ]
 
-        if(openedCards[0].type === openedCards[1].type){
-            matched();
-        } else {
-            unmatched();
+
+  const gameGrid = cardsArray
+  .concat(cardsArray)
+  .sort(() => 0.5 - Math.random());
+
+let firstGuess = '';
+let secondGuess = '';
+let count = 0;
+let previousTarget = null;
+let delay = 600;
+
+let matchedCards = 10;
+
+const game = document.getElementById('game');
+const grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+game.appendChild(grid);
+
+gameGrid.forEach(item => {
+  const { name, img } = item;
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.dataset.name = name;
+
+  const front = document.createElement('div');
+  front.classList.add('front');
+
+  const back = document.createElement('div');
+  back.classList.add('back');
+  back.style.backgroundImage = `url(${img})`;
+
+  grid.appendChild(card);
+  card.appendChild(front);
+  card.appendChild(back);
+});
+
+
+const winner = () =>{
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+
+    modal.style.display = "block";
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+        modal.style.display = "none";
         }
     }
-};
 
-function matched(){
-    openedCards[0].classList.add("match");
-    openedCards[1].classList.add("match");
-    openedCards[0].classList.remove("show", "open");
-    openedCards[1].classList.remove("show", "open");
-    openedCards = [];
-}
-
-//for when cards don't match
-function unmatched(){
-    openedCards[0].classList.add("unmatched");
-    openedCards[1].classList.add("unmatched");
-    disable();
-    setTimeout(function(){
-        openedCards[0].classList.remove("show", "open", "unmatched");
-        openedCards[1].classList.remove("show", "open", "unmatched");
-        enable();
-        openedCards = [];
-    },1100);
-}
-
-
-//disable cards temporarily
-function disable(){
-    Array.prototype.filter.call(cards, function(card){
-        card.classList.add('disabled');
-    });
-}
-
-//enable cards and disable matched cards
-function enable(){
-    Array.prototype.filter.call(cards, function(card){
-        card.classList.remove('disabled');
-        for(var i = 0; i < matchedCard.length; i++){
-            matchedCard[i].classList.add("disabled");
-        }
-    });
 }
 
 
@@ -77,76 +101,77 @@ function enable(){
 
 
 
-window.onload = startGame();
 
 
-//modal
-let modal = document.getElementById("popup1")
-//stars list
- let starsList = document.querySelectorAll(".stars li");
-//close icon in modal
- let closeicon = document.querySelector(".close");
-//congratulations when all cards match, show modal and moves, time and rating
-function congratulations(){
-    if (matchedCard.length == 16){
-        clearInterval(interval);
-        finalTime = timer.innerHTML;
-    //show congratulations modal
-    modal.classList.add("show");
-    //declare star rating variable
-    var starRating = document.querySelector(".stars").innerHTML;
-    //showing move, rating, time on modal
-    document.getElementById("finalMove").innerHTML = moves;
-    document.getElementById("starRating").innerHTML = starRating;
-    document.getElementById("totalTime").innerHTML = finalTime;
-    //closeicon on modal
-    closeModal();
-    };
-}
-
-
-//close icon on modal
-function closeModal(){
-    closeicon.addEventListener("click", function(e){
-        modal.classList.remove("show");
-        startGame();
-    });
-}
-//for player to play Again 
-function playAgain(){
-    modal.classList.remove("show");
-    startGame();
-}
-
-
-function startGame(){
+const match = () => {
+    const selected = document.querySelectorAll('.selected');
+    console.log(selected[0].childNodes[1]);
+  
  
-    // empty the openCards array
-    openedCards = [];
+    selected[0].childNodes[0].style.backgroundImage =   selected[0].childNodes[1].style.backgroundImage;;
+    selected[1].childNodes[0].style.backgroundImage =   selected[0].childNodes[1].style.backgroundImage;;
+    matchedCards +=2;
+    console.log("matchedCards" + matchedCards);
+    if(matchedCards === 12){
+        setTimeout(winner, 1400);
+    } 
 
-    // shuffle deck
-    // remove all exisiting classes from each card
-    for (var i = 0; i < cards.length; i++){
-        deck.innerHTML = "";
-        [].forEach.call(cards, function(item) {
-            deck.appendChild(item);
-        });
-        cards[i].classList.remove("show", "open", "match", "disabled");
-    }
-    // reset moves
-    moves = 0;
-    counter.innerHTML = moves;
-    // reset rating
-    for (var i= 0; i < stars.length; i++){
-        stars[i].style.color = "#FFD700";
-        stars[i].style.visibility = "visible";
-    }
-    //reset timer
-    second = 0;
-    minute = 0; 
-    hour = 0;
-    var timer = document.querySelector(".timer");
-    timer.innerHTML = "0 mins 0 secs";
-    clearInterval(interval);
-}
+    /*
 
+    selected.forEach(card => {
+        var nodeList = card.childNodes; 
+        nodeList[0].setAttribute(background, "transparent");
+
+        
+    
+      
+    });*/
+  };
+
+const resetGuesses = () => {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+  previousTarget = null;
+
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(card => {
+    card.classList.remove('selected');
+  });
+};
+
+grid.addEventListener('click', event => {
+
+  const clicked = event.target;
+
+  if (
+    clicked.nodeName === 'SECTION' ||
+    clicked === previousTarget ||
+    clicked.parentNode.classList.contains('selected') ||
+    clicked.parentNode.classList.contains('match')
+  ) {
+    return;
+  }
+
+  if (count < 2) {
+    count++;
+    if (count === 1) {
+      firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess + " " + clicked.parentNode.classList );
+      clicked.parentNode.classList.add('selected');
+    } else {
+      secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess + " " + clicked.parentNode.classList);
+      clicked.parentNode.classList.add('selected');
+    }
+
+    if (firstGuess && secondGuess) {
+      if (firstGuess === secondGuess) {
+        setTimeout(match, delay);
+      }
+      setTimeout(resetGuesses, delay);
+    }
+    previousTarget = clicked;
+  }
+
+});
